760ba790f4390042ed1ae8cbedb595f9cf1483cb
Caio Ramos <caioramos97@gmail.com>
2018-08-22 18:13:51 -0300

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import App from "./main/app"
+import ReactDOM from 'react-dom'
+import React from 'react'
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+
+ReactDOM.render(<App />,document.getElementById('app'))
