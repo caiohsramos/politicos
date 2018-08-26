d4b59b6c554db20dcf675fd1df37d76103172581
Arthur Bitencourt <arthur.fernando03@gmail.com>
2018-08-26 03:58:54 -0300

    Update about

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -7,7 +7,13 @@ import { Image } from 'react-bootstrap'
 export default props => (
     <div>
         <PageHeader title='Sobre' subtitle='' />
-        <Image src={image} responsive />
-        https://veja.abril.com.br/politica/eleicoes-2018-mapa-das-propostas/
+        <h2>Opções</h2>
+        <p>As opções disponibilizadas no quiz foram selecionadas de acordo com o mapa das propostas retirado da matéria abaixo.</p>
+        <h2>Resultado</h2>
+        <p>O resultado apresentado não tem qualquer relação com a realidade, dado que as pontuações foram selecionadas de acordo com a interpretação dos criadores.</p>
+        <h2>Motivação</h2>
+        <p>Conteúdo criado com foco no aprendizado dos desenvolvedores.</p>
+        <Image src={image} responsive height="5" width="5" />
+        <a href="https://veja.abril.com.br/politica/eleicoes-2018-mapa-das-propostas/">Mapa das propostas</a>
     </div>
 )
