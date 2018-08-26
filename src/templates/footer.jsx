fe3e7044f210f5412ab3bed82740ea55f884d182
Caio Ramos <caioramos97@gmail.com>
2018-08-26 05:07:17 -0300

    Mudancas no css

--- src/templates/footer.jsx
+++ src/templates/footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 export default props=>(
+	<div className='footer'>
 	<footer className="navbar-footer panel-footer text-center navbar-fixed-bottom">
 		<div className="glyphicon glyphicon-link">
 		<a href="https://gitlab.com/caiohsramos/politicos.git"> https://gitlab.com/caiohsramos/politicos.git</a>
 		<p>Developed by Caio & Arthur</p>
 		</div>
 	</footer>
+	</div>
 )
