17a386f79e5930536eb791241984a6c64d01c005
Arthur Bitencourt <arthur.fernando03@gmail.com>
2018-08-26 05:19:21 -0300

    update footer

--- src/templates/footer.jsx
+++ src/templates/footer.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 export default props=>(
 	<div className='footer'>
 	<footer className="navbar-footer panel-footer text-center navbar-fixed-bottom">
-		<div className="glyphicon glyphicon-link">
-		<a href="https://gitlab.com/caiohsramos/politicos"> Gitlab</a>
+		<div className="glyphicon glyphicon-link">&nbsp;
+		<a href="https://gitlab.com/caiohsramos/politicos">Gitlab</a>
+		<p>Developed by Caio & Arthur</p>
 		</div>
 	</footer>
 	</div>
