ac5a3e0ed89215a6b4ad88e6e8c55b67f483cce9
Caio Ramos <caioramos97@gmail.com>
2018-08-26 02:10:32 -0300

    Novos pacotes

--- src/quiz/tema.jsx
+++ src/quiz/tema.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Radio } from 'react-bootstrap'
 import { RadioGroup } from 'react-radio-group'
 
 import answers from './answers'
 
-export default props => {
-    const renderLines = (counter = 0) => (
-        answers[props.n-1].map(x =>(
+export default class Tema extends Component {
+
+    renderLines = (counter = 0) => (
+        answers[this.props.n-1].map(x =>(
             <div key={counter++}>
             <Radio value="" />{x.text}
             </div>)
         )
     )
     
-    return (
-        <div>
-            <RadioGroup>
-                {renderLines()}
-            </RadioGroup>
-            <Button bsStyle='primary' onClick={() => props.handleAnswer(null)}>Próximo</Button>
-        </div>
-    )
+    render () {
+        return (
+            <div>
+                <RadioGroup>
+                    {this.renderLines()}
+                </RadioGroup>
+                <Button bsStyle='primary' onClick={() => this.props.handleAnswer(null)}>Próximo</Button>
+            </div>
+        )
+    }
 }
 
 
