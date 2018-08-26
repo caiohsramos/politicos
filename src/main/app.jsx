1652a7a0c0461094dc09805faf38e0480bcd36c8
Arthur Bitencourt <arthur.fernando03@gmail.com>
2018-08-26 04:44:00 -0300

    Add footer

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Menu from '../templates/menu'
 import Routes from './routes'
+import Footer from '../templates/footer'
 
 export default props => (
     <div className='container'>
         <Menu />
         <Routes />
+        <Footer />
     </div>
 )
---
+++ src/templates/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default props=>(
+	<footer className="navbar-footer panel-footer text-center navbar-fixed-bottom">
+		<div className="glyphicon glyphicon-link">
+		<a href="https://gitlab.com/caiohsramos/politicos.git"> https://gitlab.com/caiohsramos/politicos.git</a>
+		<p>Developed by Caio & Arthur</p>
+		</div>
+	</footer>
+)
