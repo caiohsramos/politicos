d0978ffca3733e3f60867c6c2f840754f9044311
Caio Ramos <caioramos97@gmail.com>
2018-08-25 21:42:39 -0300

    Mudancas na pagina de politicos

--- src/politicos/cardsView.jsx
+++ src/politicos/cardsView.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Row, Col, Thumbnail, Button } from 'react-bootstrap'
+import { Grid, Row, Col, Thumbnail, Button, Label } from 'react-bootstrap'
 
 import alvaro from '../images/alvaro-dias.jpg'
 import alckmin from '../images/geraldo-alckmin.jpg'
@@ -16,34 +16,34 @@ export default props => (
         <Row>
             <Col xs={9} md={4}>
                 <Thumbnail src={alvaro} alt="242x200">
-                    <h3>Thumbnail label</h3>
-                    <p>Description</p>
+                    <h3>Alvaro Fernandes Dias</h3>
+                    <h4>Podemos <Label>19</Label></h4>
                     <p>
-                        <Button bsStyle="primary">Button</Button>
+                        <Button bsStyle="info">Página do Candidato</Button>
                         &nbsp;
-                        <Button bsStyle="default">Button</Button>
+                        <Button bsClass='btn btn-default pull-right'>Ficha Limpa</Button>
                     </p>
                 </Thumbnail>
             </Col>
             <Col xs={9} md={4}>
                 <Thumbnail src={ciro} alt="242x200">
-                    <h3>Thumbnail label</h3>
-                    <p>Description</p>
+                <h3>Ciro Ferreira Gomes</h3>
+                    <h4>PDT <Label>12</Label></h4>
                     <p>
-                        <Button bsStyle="primary">Button</Button>
+                        <Button bsStyle="info">Página do Candidato</Button>
                         &nbsp;
-                        <Button bsStyle="default">Button</Button>
+                        <Button bsClass='btn btn-default pull-right'>Ficha Limpa</Button>
                     </p>
                 </Thumbnail>
             </Col>
             <Col xs={9} md={4}>
                 <Thumbnail src={alckmin} alt="242x200">
-                    <h3>Thumbnail label</h3>
-                    <p>Description</p>
+                <h3>Geraldo José Rodrigues Alckmin Filho</h3>
+                    <h4>PSDB <Label>45</Label></h4>
                     <p>
-                        <Button bsStyle="primary">Button</Button>
+                        <Button bsStyle="info">Página do Candidato</Button>
                         &nbsp;
-                        <Button bsStyle="default">Button</Button>
+                        <Button bsClass='btn btn-default pull-right'>Ficha Limpa</Button>
                     </p>
                 </Thumbnail>
             </Col>
@@ -52,34 +52,34 @@ export default props => (
         <Row>
             <Col xs={9} md={4}>
                 <Thumbnail src={boulos} alt="242x200">
-                    <h3>Thumbnail label</h3>
-                    <p>Description</p>
+                <h3>Guilherme Castro Boulos</h3>
+                    <h4>PSol <Label>50</Label></h4>
                     <p>
-                        <Button bsStyle="primary">Button</Button>
+                        <Button bsStyle="info">Página do Candidato</Button>
                         &nbsp;
-                        <Button bsStyle="default">Button</Button>
+                        <Button bsClass='btn btn-default pull-right'>Ficha Limpa</Button>
                     </p>
                 </Thumbnail>
             </Col>
             <Col xs={9} md={4}>
                 <Thumbnail src={meirelles} alt="242x200">
-                    <h3>Thumbnail label</h3>
-                    <p>Description</p>
+                <h3>Henrique de Campos Meirelles</h3>
+                    <h4>MDB <Label>15</Label></h4>
                     <p>
-                        <Button bsStyle="primary">Button</Button>
+                        <Button bsStyle="info">Página do Candidato</Button>
                         &nbsp;
-                        <Button bsStyle="default">Button</Button>
+                        <Button bsClass='btn btn-default pull-right'>Ficha Limpa</Button>
                     </p>
                 </Thumbnail>
             </Col>
             <Col xs={9} md={4}>
                 <Thumbnail src={bolsonaro} alt="242x200">
-                    <h3>Thumbnail label</h3>
-                    <p>Description</p>
+                <h3>Jair Messias Bolsonaro</h3>
+                    <h4>PSC <Label>20</Label></h4>
                     <p>
-                        <Button bsStyle="primary">Button</Button>
+                        <Button bsStyle="info">Página do Candidato</Button>
                         &nbsp;
-                        <Button bsStyle="default">Button</Button>
+                        <Button bsClass='btn btn-default pull-right'>Ficha Limpa</Button>
                     </p>
                 </Thumbnail>
             </Col>
@@ -88,34 +88,34 @@ export default props => (
         <Row>
             <Col xs={9} md={4}>
                 <Thumbnail src={amoedo} alt="242x200">
-                    <h3>Thumbnail label</h3>
-                    <p>Description</p>
+                <h3>João Dionisio Filgueira Barreto Amoêdo</h3>
+                    <h4>Novo <Label>30</Label></h4>
                     <p>
-                        <Button bsStyle="primary">Button</Button>
+                        <Button bsStyle="info">Página do Candidato</Button>
                         &nbsp;
-                        <Button bsStyle="default">Button</Button>
+                        <Button bsClass='btn btn-default pull-right'>Ficha Limpa</Button>
                     </p>
                 </Thumbnail>
             </Col>
             <Col xs={9} md={4}>
                 <Thumbnail src={marina} alt="242x200">
-                    <h3>Thumbnail label</h3>
-                    <p>Description</p>
+                <h3>Maria Osmarina da Silva Vaz de Lima</h3>
+                    <h4>Rede <Label>18</Label></h4>
                     <p>
-                        <Button bsStyle="primary">Button</Button>
+                        <Button bsStyle="info">Página do Candidato</Button>
                         &nbsp;
-                        <Button bsStyle="default">Button</Button>
+                        <Button bsClass='btn btn-default pull-right'>Ficha Limpa</Button>
                     </p>
                 </Thumbnail>
             </Col>
             <Col xs={9} md={4}>
                 <Thumbnail src={lula} alt="242x200">
-                    <h3>Thumbnail label</h3>
-                    <p>Description</p>
+                <h3>Luiz Inácio Lula da Silva</h3>
+                    <h4>PT <Label>13</Label></h4>
                     <p>
-                        <Button bsStyle="primary">Button</Button>
+                        <Button bsStyle="info">Página do Candidato</Button>
                         &nbsp;
-                        <Button bsStyle="default">Button</Button>
+                        <Button bsClass='btn btn-default pull-right'>Ficha Limpa</Button>
                     </p>
                 </Thumbnail>
             </Col>
