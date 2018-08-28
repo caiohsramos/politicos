911f9fda9cfc8be55f02fe0a7c6f9f1b4bb0f286
Caio Ramos <caioramos97@gmail.com>
2018-08-27 23:16:49 -0300

    Remodelando footer

--- src/templates/footer.jsx
+++ src/templates/footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export default props=>(
-	<footer className="navbar-footer panel-footer text-center navbar-fixed-bottom">
+	<div className='footer'>
+	<footer className="navbar-footer panel-footer text-center navbar-fixed-bottom" id='footer'>
 		<spam className="glyphicon glyphicon-link"/>&nbsp;
 		<a href="https://gitlab.com/caiohsramos/politicos">Gitlab</a>
 		<p>Developed by Caio & Arthur</p>
 	</footer>
+	</div>
 )
